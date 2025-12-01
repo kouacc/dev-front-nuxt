19a1f6a01b9d1b073ad9455261d75601c3e9d3b1
kouacc <maxrur57@gmail.com>
2025-12-01 16:42:37 +0100

    feat(recipe): show recipe details

---
+++ app/pages/recipe/[id].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'aside'
+})
+
+const { id } = useRoute().params
+
+const { data: recipe, error } = await useAsyncData(`recipe-${id}`, async () => {
+  const { data } = await $fetch<{ data: Recipe }>(`/recipes/${id}`, {
+    baseURL: useRuntimeConfig().public.apiUrl
+  })
+  return data
+})
+
+if (error.value) {
+  throw createError({
+    statusCode: error.value.statusCode || 500,
+    statusMessage: error.value.statusMessage || 'Erreur lors du chargement de la recette.'
+  })
+}
+</script>
+
+<template>
+  <div>
+    <UITitle>{{ recipe?.title }}</UITitle>
+    <pre>{{ recipe }}</pre>
+  </div>
+</template> 
