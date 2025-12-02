f357fa644eb1160237f29d53ec8959e779608bf9
kouacc <maxrur57@gmail.com>
2025-12-02 09:04:15 +0100

    fix(icons): replace breadcrumb chevron with icon component

--- app/components/UIBreadcrumb.vue
+++ app/components/UIBreadcrumb.vue
@@ -17,17 +17,7 @@ defineProps<{
           <Icon v-if="item.icon" :name="item.icon" />
           <span :class="{ 'ui-breadcrumb__active': !item.href, 'ui-breadcrumb__disabled': item.disabled }">{{ item.text }}</span>
         </template>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+        <Icon name="lucide:chevron-right" />
       </li>
     </ol>
   </nav>
