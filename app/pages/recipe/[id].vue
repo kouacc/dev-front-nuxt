f79e24ed57ebd3bd83de5a8ee4364142e57e7857
kouacc <maxrur57@gmail.com>
2025-12-02 08:59:48 +0100

    feat(api): list ingredients and instructions

--- app/pages/recipe/[id].vue
+++ app/pages/recipe/[id].vue
@@ -23,6 +23,21 @@ if (error.value) {
 <template>
   <div>
     <UITitle>{{ recipe?.title }}</UITitle>
-    <pre>{{ recipe }}</pre>
+    <div>
+      <UITitle type="heading2">Ingrédients</UITitle>
+      <ul>
+        <li v-for="(ing, index) in recipe?.ingredients" :key="index">
+          {{ ing.quantity }} {{ ing.unit }} {{ ing.name }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <UITitle type="heading2">Etapes</UITitle>
+      <ul>
+        <li v-for="(step, index) in recipe?.instructions" :key="index">
+          {{ step.step_number }}. {{ step.description }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template> 
