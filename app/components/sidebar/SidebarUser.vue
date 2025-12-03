07a00f9aacaf08a56fbb7c08fcd652fc1e3c0f45
kouacc <maxrur57@gmail.com>
2025-12-03 14:18:20 +0100

    style(sidebar): user details styles

--- app/components/sidebar/SidebarUser.vue
+++ app/components/sidebar/SidebarUser.vue
@@ -11,3 +11,17 @@ const user = useAuth().getUser()
     <button @click="useAuth().logout"><Icon name="lucide:log-out" size="20" /></button>
   </div>
 </template> 
+
+<style lang="scss">
+.sidebar-user {
+  --sd-user-bg-color: var(--color-white);
+
+  background-color: var(--sd-user-bg-color);
+  padding: rem(8);
+  border-radius: rem(12);
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+
+}
+</style>
