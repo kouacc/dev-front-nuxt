4e4b3df65af8a0249caa3545b7ca2bfc6368a435
kouacc <maxrur57@gmail.com>
2025-12-03 14:26:51 +0100

    feat(avatar): avatar component

---
+++ app/components/ui/UIAvatar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  src?: string;
+}>()
+</script>
+
+<template>
+  <div class="ui-avatar">
+    <NuxtImg v-if="src" :src="src" alt="Avatar" />
+    <div v-else class="ui-avatar--placeholder">
+      {{ name.slice(0, 2).toUpperCase() }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.ui-avatar {
+  --avatar-bg-color: var(--color-primary-500);
+
+  border-radius: rem(9999);
+  width: rem(40);
+  height: rem(40);
+  object-fit: cover;
+  background-color: var(--avatar-bg-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &--placeholder {
+    font-weight: 600;
+    font-size: rem(16);
+    color: var(--color-white);
+  }
+}
+</style>
