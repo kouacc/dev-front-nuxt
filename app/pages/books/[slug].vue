a97679126c55e943b154e0a85755e0759f7a16bb
kouacc <maxrur57@gmail.com>
2025-12-08 15:50:41 +0100

    refactor(cms): use image composable

--- app/pages/books/[slug].vue
+++ app/pages/books/[slug].vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import type { SanityDocument } from '@sanity/client'
-import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url'
+import type { SanityBook } from '~/types/cms/book'
 
-const query = groq`*[_type == "book" && slug.current == $slug][0]`
+const query = groq`*[_type == "book" && slug.current == $slug][0]{title, slug, publishedAt, cover, author->{ name }, body}`
 
-const { data: book } = await useLazySanityQuery<SanityDocument>(query, useRoute().params)
-
-const { projectId, dataset } = useSanity().client.config()
-const urlFor = (source: SanityImageSource) =>
-  projectId && dataset
-    ? createImageUrlBuilder({ projectId, dataset }).image(source)
-    : null
-
+const { data: book } = await useLazySanityQuery<SanityBook>(query, useRoute().params)
 /* if (!book.value) {
   throw createError({ statusCode: 404, statusMessage: 'Book not found' })
 }  */
@@ -19,7 +11,8 @@ const urlFor = (source: SanityImageSource) =>
 
 <template>
   <div v-if="book">
-    <pre>{{ book }}</pre>
-    <NuxtImg v-if="book.cover" :src="urlFor(book.cover).url()" :alt="book?.title" />
+    <UITitle tag="h1">{{ book.title }}</UITitle>
+    <NuxtImg v-if="book.cover" :src="useSanityImage(book.cover)?.url()" :alt="book?.title" />
+    <SanityContent v-if="book.body" :value="book.body" />
   </div>
 </template>
--- app/pages/books/index.vue
+++ app/pages/books/index.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import type { SanityDocument } from '@sanity/client'
 
-const BOOKS_QUERY = groq`*[ _type == "book"]|order(publishedAt desc)[0...10]{_id, title, slug, publishedAt}`
-
-const { data: books } = await useLazySanityQuery<SanityDocument[]>(BOOKS_QUERY)
-console.log(books.value)
+const BOOKS_QUERY = groq`*[ _type == "book"]|order(publishedAt desc)[0...10]{title, slug, publishedAt, cover, author->{ name }, categories[]->{ ... }}`
+const { data: books } = await useLazySanityQuery<SanityBook[]>(BOOKS_QUERY)
 </script>
 
 <template>
   <div>
     <UITitle tag="h1">Liste des livres</UITitle>
-    <pre>{{ books }}</pre>
     <NuxtLink v-for="book in books" :key="book._id" :to="`/books/${book.slug.current}`">
       <div>
+        <NuxtImg v-if="book.cover" :src="useSanityImage(book.cover)?.url()" :alt="book?.title" />
         <h2>{{ book.title }}</h2>
         <p>Publié le {{ new Date(book.publishedAt).toLocaleDateString() }}</p>
+        <p>par {{ book.author.name }}</p>
       </div>
     </NuxtLink>
+    <pre>{{ books }}</pre>
   </div>
 </template>
