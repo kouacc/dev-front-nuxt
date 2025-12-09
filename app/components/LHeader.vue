8e431500370f78799ba5632e8e8ffc211cb0343c
kouacc <maxrur57@gmail.com>
2025-12-09 14:52:57 +0100

    feat(header): add dynamic navigation through cms

---
+++ app/components/LHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { SanitySiteSettings } from '~/types/cms/settings'
+
+defineProps<{
+  nav: SanitySiteSettings['navigation'] | null;
+}>()
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="(l, index) in nav" :key="index">
+          <NuxtLink :to="l.link">{{ l.label }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,18 @@
+<script setup lang="ts">
+const nav_query = groq`*[_type == "siteSettings"][0]{title, description, mainNavigation}`
+
+const { data: settings } = await useLazySanityQuery<SanitySiteSettings>(nav_query)
+
+
+useSeoMeta({
+  title: () => settings?.value?.title || 'Recettes',
+  description: () => settings?.value?.description || 'Manage your recipes and goals'
+})
+</script>
+
 <template>
+  <LHeader :nav="settings?.mainNavigation" />
+  <!-- eslint-disable-next-line vue/no-multiple-template-root -->
   <main class="container">
     <slot />
   </main>
