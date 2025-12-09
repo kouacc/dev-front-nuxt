edf5ba1dc141aa7800c21e708928aace8d4a3a2c
kouacc <maxrur57@gmail.com>
2025-12-09 16:11:24 +0100

    style(header): add global header styles

--- app/components/LHeader.vue
+++ app/components/LHeader.vue
@@ -4,16 +4,73 @@ import type { SiteSettings } from '~/types/sanity.types'
 defineProps<{
   nav: SiteSettings['mainNavigation']
 }>()
+
+const isLoggedin = computed(() => useAuth().getUser !== null)
 </script>
 
 <template>
-  <header>
+  <header class="m-header">
     <nav>
       <ul>
         <li v-for="(l, index) in nav" :key="index">
-          <NuxtLink :to="l.link">{{ l.label }}</NuxtLink>
+          <NuxtLink :to="l.link" class="m-header__navlink">{{ l.label }}</NuxtLink>
         </li>
       </ul>
+      <div class="m-header__auth">
+        <UIButton v-if="isLoggedin" size="small" as="link" to="/dashboard">Mon dashboard</UIButton>
+        <UIButton size="small" as="link" to="/login" variant="ghost">Se connecter</UIButton>
+        <UIButton size="small" as="link" to="/signup">Créer un compte</UIButton>
+      </div>
     </nav>
   </header>
 </template>
+
+<style lang="scss">
+.m-header {
+  --mheader-bg-color: var(--color-white);
+  --mheader-border-color: var(--color-grey-300);
+  --mheader-text-color: var(--color-text);
+
+  background-color: var(--mheader-bg-color);
+  border-bottom: 1.5px solid var(--mheader-border-color);
+  color: var(--mheader-text-color);
+  padding: rem(12);
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    gap: rem(12);
+
+    > li {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  &__auth {
+    display: flex;
+    gap: rem(8);
+  }
+
+  &__navlink {
+    font-weight: 600;
+    color: var(--mheader-text-color);
+    text-decoration: none;
+    padding: rem(12) rem(18);
+    border-radius: rem(9999);
+    transition: background-color 0.1s ease-in-out;
+
+    &:hover {
+      background-color: var(--color-primary-100);
+    }
+  }
+}
+</style>
