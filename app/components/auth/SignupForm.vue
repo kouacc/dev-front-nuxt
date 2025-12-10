ffc0a931af64993f8c2d1c8f4d56c31bc33615a7
kouacc <maxrur57@gmail.com>
2025-12-10 15:41:45 +0100

    feat(signup): signup form component

---
+++ app/components/auth/SignupForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const form = reactive<RegisterPayload & { confirm_password: string }>({
+  email: '',
+  password: '',
+  confirm_password: '',
+  first_name: '',
+  last_name: '',
+  username: '',
+  is_admin: false
+})
+</script>
+<template>
+  <form class="register-form" @submit.prevent="onSubmitSignup">
+    <UIInput v-model="form.first_name" label="Prénom" type="text" required />
+    <UIInput v-model="form.last_name" label="Nom" type="text" required />
+    <UIInput v-model="form.username" label="Nom d'utilisateur" type="text" required />
+    <UIInput v-model="form.email" label="Adresse e-mail" type="email" required />
+    <UIInput v-model="form.password" label="Mot de passe" type="password" :color="passwordInputColor" required />
+
+    <UIButton as="button" type="submit">S'inscrire</UIButton>
+  </form>
+</template>
