9291fe09d0b40c2d19fc32260e24158325853e88
kouacc <maxrur57@gmail.com>
2025-12-10 15:43:37 +0100

    feat(signup): signup page

---
+++ app/pages/signup.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div class="p-signup">
+    <section>
+      <UITitle>Inscription</UITitle>
+      <p>Créez un compte pour accéder à toutes les fonctionnalités.</p>
+      <NuxtLink to="/login" class="p-signup__login">Vous avez un compte ? Connectez-vous</NuxtLink>
+    </section>
+    <SignupForm />
+  </div>
+</template>
