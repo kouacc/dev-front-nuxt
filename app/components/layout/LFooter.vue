d2fb8ee20e892a3387dc6d0ae698b27a8764725b
kouacc <maxrur57@gmail.com>
2025-12-22 19:32:06 +0100

    feat(footer): init footer and footer nav

---
+++ app/components/layout/LFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="m-footer">
+    <LFooterNav v-for="(n, index) in props.nav" :key="index" :menu="n" />
+  </footer>
+</template>
+
+<style lang="scss">
+.m-footer {
+  margin-top: rem(60);
+  padding: rem(20) 0;
+  text-align: center;
+  font-size: rem(14);
+  color: var(--color-text-secondary);
+  border-top: 1px solid var(--color-grey-200);
+}
+</style>
---
+++ app/components/layout/LFooterNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <span>{{ props.menu.title }}</span>
+    <ul>
+      <li v-for="(i, index) in props.menu.items" :key="index">
+        <NuxtLink :to="i.link">{{ i.label }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
