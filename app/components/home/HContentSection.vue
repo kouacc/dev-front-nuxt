5302d36a016bb6b99f7ca086457c6306d9baa7af
kouacc <maxrur57@gmail.com>
2025-12-22 11:03:34 +0100

    feat(content): move content item to component

---
+++ app/components/home/HContentSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Homepage } from '~/types/sanity.types'
+
+const props = defineProps<{
+  content: NonNullable<Homepage['content']>[number]
+}>()
+
+</script>
+
+<template>
+  <div class="h-section">
+    <div class="h-section__content">
+      <SanityContent :value="props.content.sectionBody" />
+    </div>
+    <!-- <NuxtImg v-if="props.content.sectionImage" class="h-section__img" :src="useSanityImage(props.content.sectionImage)?.url()" /> -->
+    <NuxtImg class="h-section__img" src="https://placehold.co/600x400" />
+  </div>
+</template>
