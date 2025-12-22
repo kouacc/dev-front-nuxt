90fa9602e25396157ce71f601d74e6b6224f0b0d
kouacc <maxrur57@gmail.com>
2025-12-22 11:04:32 +0100

    feat(content): list content section

---
+++ app/components/home/HContentSectionList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Homepage } from '~/types/sanity.types'
+
+const props = defineProps<{
+  title: NonNullable<Homepage['contentTitle']>,
+  content: NonNullable<Homepage['content']>
+}>()
+</script>
+
+<template>
+  <div>
+    <UITitle type="heading2" >{{ props.title }}</UITitle>
+    <HContentSection
+      v-for="(section, index) in props.content"
+      :key="index"
+      :content="section"
+    />
+  </div>
+</template>
