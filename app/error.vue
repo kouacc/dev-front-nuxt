9e3d2be4d29f9ca4da9ec6f2e2b947b0ea64fff9
kouacc <maxrur57@gmail.com>
2025-12-23 15:19:32 +0100

    feat(error): create error page and define template

---
+++ app/error.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  // eslint-disable-next-line vue/require-default-prop
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <div class="error-page">
+    <div class="error-page__content">
+      <span class="error-page__code">{{ error?.statusCode || 500 }}</span>
+      <UITitle type="heading1" align="center">
+        {{ error?.message || 'Une erreur est survenue' }}
+      </UITitle>
+
+      <UIButton class="error-page__btn" as="link" to="/" variant="default" size="medium">
+        <Icon name="lucide:home" />
+        Retour à l'accueil
+      </UIButton>
+
+    </div>
+  </div>
+</template>
