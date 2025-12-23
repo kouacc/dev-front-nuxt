0c5a04fa71960a57e04a4575f5368fe4b7a80ade
kouacc <maxrur57@gmail.com>
2025-12-23 15:02:52 +0100

    style(footer): footer navigation styles

--- app/components/layout/LFooterNav.vue
+++ app/components/layout/LFooterNav.vue
@@ -17,3 +17,36 @@ const props = defineProps<{
     </ul>
   </nav>
 </template>
+
+<style lang="scss">
+.m-footer-nav {
+  flex-grow: 0;
+  max-width: rem(300);
+
+  &__title {
+    display: block;
+    font-weight: 600;
+    margin-bottom: rem(8);
+  }
+
+  &__list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  &__item {
+    margin-bottom: rem(4);
+  }
+
+  &__link {
+    color: var(--color-text-secondary);
+    text-decoration: none;
+    transition: color 0.1s ease-in-out;
+
+    &:hover {
+      color: var(--color-primary);
+    }
+  }
+}
+</style>
