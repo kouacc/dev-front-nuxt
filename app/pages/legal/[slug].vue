724bfa3d2e9f58f6303cdce1e1805e1a19f1d80b
kouacc <maxrur57@gmail.com>
2025-12-25 12:13:34 +0100

    feat(legal): query sanity

--- app/pages/legal/[slug].vue
+++ app/pages/legal/[slug].vue
@@ -2,6 +2,10 @@
 import type { LegalType } from '~/types/sanity.types'
 
 const { slug } = useRoute().params
+
+const query = groq`*[_id == "${slug}"][0]`
+
+const { data: page } = await useLazySanityQuery<LegalType>(query)
 </script>
 
 <template>
