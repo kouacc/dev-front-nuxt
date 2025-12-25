63aa2f6b981f4f6473f07c153ee01ca5f0c5b731
kouacc <maxrur57@gmail.com>
2025-12-25 12:13:04 +0100

    feat(legal): legal page using slug

---
+++ app/pages/legal/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <UITitle>{{ page?.title }}</UITitle>
+    <p>Dernière mise à jour : {{ page?.lastUpdated }}</p>
+    <SanityContent :value="page?.content" />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
