df3a9212c21c0ed491be73fe61277a75836b8642
kouacc <maxrur57@gmail.com>
2025-12-28 11:59:49 +0100

    style(option): style custom select option

--- app/components/ui/UISelectOption.vue
+++ app/components/ui/UISelectOption.vue
@@ -10,3 +10,19 @@ const props = defineProps<{
     <slot />
   </option>
 </template>
+
+<style lang="scss">
+.ui-soption {
+  /* display: inline-flex; */
+  justify-content: flex-start;
+  gap: rem(16);
+  padding: rem(6) rem(12);
+  transition: 0.1s background-color;
+  border-radius: #{rem(6)};
+
+  &::checkmark {
+    order: 1;
+    margin-left: auto;
+  }
+}
+</style>
