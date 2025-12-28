c4d1036ca5fca4aa78c9f1d5a5342ffc3938d522
kouacc <maxrur57@gmail.com>
2025-12-28 11:59:25 +0100

    feat(option): custom option component

---
+++ app/components/ui/UISelectOption.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  icon?: string
+}>()
+</script>
+
+<template>
+  <option class="ui-soption" v-bind="$attrs">
+    <Icon v-if="props.icon" size="18" :name="props.icon"  />
+    <slot />
+  </option>
+</template>
