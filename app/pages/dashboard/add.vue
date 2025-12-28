b79045e6926bc75a8c352afec1dfe507d06458fb
kouacc <maxrur57@gmail.com>
2025-12-28 14:55:42 +0100

    feat(recipe): create recipe page

---
+++ app/pages/dashboard/add.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+  layout: 'aside'
+})
+
+</script>
+
+<template>
+  <div>
+    <UITitle>Créer une nouvelle recette</UITitle>
+    <div class="p-dashboard-add">
+      <AddRecipeForm />
+      <AddInstructionForm recipe-id="1" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.p-dashboard-add {
+  display: flex;
+  gap: rem(32);
+}
+</style>
