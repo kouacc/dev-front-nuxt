0f2fa289f3783b206557ce5a729548a61b794e19
kouacc <maxrur57@gmail.com>
2025-12-29 17:16:53 +0100

    feat(form): create ingredient form

---
+++ app/components/AddIngredientForm.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  recipeId: number | null
+}>()
+
+const toast = useToast()
+
+const { data: ingredients } = await useAsyncData('ingredients', async () => {
+  const { data } = await $fetch<APIResponse<Ingredient[]>>('/ingredients', {
+    baseURL: useRuntimeConfig().public.apiUrl
+  })
+  return data
+})
+</script>
+
+<template>
+</template>
