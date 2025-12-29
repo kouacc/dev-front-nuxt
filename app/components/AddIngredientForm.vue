efbfbc4045bddf40a86be4dfeeefeb80909c89b1
kouacc <maxrur57@gmail.com>
2025-12-29 17:56:16 +0100

    feat(form): add ingredient template

--- app/components/AddIngredientForm.vue
+++ app/components/AddIngredientForm.vue
@@ -14,4 +14,25 @@ const { data: ingredients } = await useAsyncData('ingredients', async () => {
 </script>
 
 <template>
+  <form class="fr-addingredient" @submit.prevent="onSubmit">
+    <div class="fr-addingredient__title">
+      <UITitle type="heading2">Ingrédients</UITitle>
+      <UIButton type="button" size="small" @click="addIngredient">
+        <Icon name="lucide:plus" />Ajouter
+      </UIButton>
+    </div>
+    <div v-for="(item, index) in selectedIngredients" :key="index" class="fr-addingredient__row">
+      <UISelect v-model="item.ingredient_id" label="Ingrédient" class="fr-addingredient__row__select">
+        <UISelectOption :value="0" disabled>Choisir un ingrédient</UISelectOption>
+        <UISelectOption v-for="ingredient in ingredients" :key="ingredient.ingredient_id" :value="ingredient.ingredient_id">
+          {{ ingredient.name }} ({{ ingredient.unit }})
+        </UISelectOption>
+      </UISelect>
+      <UIInput v-model="item.quantity" type="number" label="Quantité" min="1" class="fr-addingredient__row__quantity" />
+      <UIButton type="button" size="small" variant="ghost" :disabled="selectedIngredients.length === 1" @click="removeIngredient(index)">
+        <Icon name="lucide:x" />
+      </UIButton>
+    </div>
+    <UIButton as="button" type="submit">Envoyer</UIButton>
+  </form>
 </template>
