7f9e29ae934c9b8516474aa11800e820ff69e4ba
kouacc <maxrur57@gmail.com>
2025-12-29 17:18:31 +0100

    feat(add): add page wrapper and change to grid layout

--- app/pages/dashboard/add.vue
+++ app/pages/dashboard/add.vue
@@ -17,8 +17,31 @@ definePageMeta({
 </template>
 
 <style lang="scss">
-.p-dashboard-add {
+.p-dashboard-add-wrapper {
   display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+}
+
+.p-dashboard-add {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   gap: rem(32);
+  margin-top: rem(12);
+  flex: 1;
+  min-height: 0;
+  overflow: hidden;
+
+  &__left {
+    display: flex;
+    flex-direction: column;
+    gap: rem(32);
+    overflow-y: auto;
+  }
+
+  &__right {
+    overflow-y: auto;
+  }
 }
 </style>
