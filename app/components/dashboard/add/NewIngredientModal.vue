93c34875c99dd13e42848a3e059e62d41df343a0
kouacc <maxrur57@gmail.com>
2025-12-29 20:13:52 +0100

    feat(form): create ingredient modal

---
+++ app/components/dashboard/add/NewIngredientModal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const open = defineModel<boolean>('open', { default: false }) 
+const ingredients = defineModel<Omit<Ingredient, 'quantity'>[]>('ingredients', { default: [] })
+
+const toast = useToast()
+
+const formContent = reactive({
+  name: '',
+  unit: '',
+  clear (): void {
+    this.name = ''
+    this.unit = ''
+  }
+})
+</script>
+
+<template>
+  <UIModal v-model="open" title="Créer un nouvel ingrédient">
+    <template #content>
+      <form class="md-newingredient" @submit.prevent="onSubmit()">
+      </form>
+    </template>
