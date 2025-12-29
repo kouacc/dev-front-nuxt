d36f97bb4d56d94784379f99b832cc32db6eb1e5
kouacc <maxrur57@gmail.com>
2025-12-29 18:32:47 +0100

    feat(modal): props model and template

---
+++ app/components/ui/UIModal.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+
+const open = defineModel<boolean>({ default: false });
+</script>
+
+<template>
+    <div v-if="open" class="ui-modal">
+        <div v-if="open" class="ui-modal-dialog">
+          <div class="ui-modal-dialog__header">
+            <UITitle type="heading2">{{ props.title }}</UITitle>
+          </div>
+
+          <div class="ui-modal-dialog__content">
+            <slot name="content" />
+          </div>
+          <div class="ui-modal-dialog__footer">
+            <slot name="footer" />
+          </div>
+        </div>
+    </div>
+</template>
