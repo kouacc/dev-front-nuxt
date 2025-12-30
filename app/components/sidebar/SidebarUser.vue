51112157c23ec5f3936fcd7ac63b647d8f20c1c8
kouacc <maxrur57@gmail.com>
2025-12-30 19:33:26 +0100

    fix(sidebar): set logout button to text color var

--- app/components/sidebar/SidebarUser.vue
+++ app/components/sidebar/SidebarUser.vue
@@ -10,7 +10,11 @@ const user = useAuth().getUser()
       <UIAvatar :name="user!.username" />
       <span>{{ user?.username }}</span>
     </div>
-    <UITooltip content="Déconnexion" :delay="1000"><button class="sidebar-user__btn" @click="useAuth().logout"><Icon name="lucide:log-out" size="20" /></button></UITooltip>
+    <UITooltip content="Déconnexion" :delay="1000">
+      <button class="sidebar-user__btn" @click="useAuth().logout">
+        <Icon name="lucide:log-out" size="20" />
+      </button>
+    </UITooltip>
   </div>
 </template> 
 
@@ -41,6 +45,7 @@ const user = useAuth().getUser()
     --sd-user-btn-color: transparent;
 
     background: var(--sd-user-btn-color);
+    color: var(--color-text);
     border: none;
     cursor: pointer;
     padding: 0;
