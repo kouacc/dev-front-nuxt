b843a4cce70664629c6d7d79d50dcaaee2c8076d
kouacc <maxrur57@gmail.com>
2025-12-30 18:43:58 +0100

    feat(cuisine): handle api delete

--- app/components/dashboard/admin/cuisines/DeleteCuisineModal.vue
+++ app/components/dashboard/admin/cuisines/DeleteCuisineModal.vue
@@ -5,6 +5,39 @@ const cuisines = defineModel<Cuisine[]>('cuisines', { required: true })
 
 const toast = useToast()
 
+const deleteCuisine = async () => {
+  await $fetch(`/cuisines/${cuisineData.value.cuisine_id}`, {
+    method: 'DELETE',
+    baseURL: useRuntimeConfig().public.apiUrl,
+    headers: {
+      Authorization: `Bearer ${useCookie('user-token').value}`
+    },
+    onResponse ({ response }) {
+      if (response.status !== 200) {
+        toast.addToast({
+          title: 'Erreur',
+          message: response._data.message || 'Une erreur est survenue lors de la suppression de la cuisine.',
+          type: 'error'
+        })
+        return
+      }
+      toast.addToast({
+        title: 'Cuisine supprimée',
+        message: 'La cuisine a bien été supprimée.',
+        type: 'success'
+      })
+      
+      const index = cuisines.value.findIndex(c => c.cuisine_id === cuisineData.value.cuisine_id)
+      if (index !== -1) {
+        cuisines.value.splice(index, 1)
+      }
+
+      open.value = false
+      cuisineData.value = {} as Cuisine
+    }
+  })
+}
+
 const onClose = () => {
   open.value = false
   cuisineData.value = {} as Cuisine
