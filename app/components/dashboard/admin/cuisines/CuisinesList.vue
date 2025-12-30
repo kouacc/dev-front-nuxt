55defc796150594c05487783d68623eb8cab5f91
kouacc <maxrur57@gmail.com>
2025-12-30 18:47:51 +0100

    feat(cuisine): cuisines list

---
+++ app/components/dashboard/admin/cuisines/CuisinesList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { data: cuisines } = await useAsyncData('cuisines', async () => {
+  const { data } = await $fetch<APIResponse<Cuisine[]>>('/cuisines', {
+    baseURL: useRuntimeConfig().public.apiUrl
+  })
+  return data
+})
+</script>
+<template>
+  <div class="cuisines-list">
+  </div>
+</template>
