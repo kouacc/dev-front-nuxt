fd5d3d0cb05c88791037b3485d9bdfbbbffae486
kouacc <maxrur57@gmail.com>
2025-12-30 18:46:41 +0100

    feat(cuisine): handle api update

--- app/components/dashboard/admin/cuisines/UpdateCuisineModal.vue
+++ app/components/dashboard/admin/cuisines/UpdateCuisineModal.vue
@@ -9,6 +9,61 @@ const formContent = reactive({
   name: ''
 })
 
+const updateCuisine = async () => {
+  if (!formContent.name.trim()) {
+    toast.addToast({
+      title: 'Erreur',
+      message: 'Le nom de la cuisine est requis.',
+      type: 'error'
+    })
+    return
+  }
+
+  if (formContent.name.trim() === cuisineData.value.name) {
+    formContent.name = ''
+    open.value = false
+    return
+  }
+
+  await $fetch(`/cuisines/${cuisineData.value.cuisine_id}`, {
+    method: 'PUT',
+    baseURL: useRuntimeConfig().public.apiUrl,
+    headers: {
+      Authorization: `Bearer ${useCookie('user-token').value}`
+    },
+    body: {
+      name: formContent.name.trim()
+    },
+    onResponse ({ response }) {
+      if (response.status !== 200) {
+        toast.addToast({
+          title: 'Erreur',
+          message: 'Une erreur est survenue lors de la mise à jour de la cuisine.',
+          type: 'error'
+        })
+        return
+      }
+      toast.addToast({
+        title: 'Cuisine mise à jour',
+        message: 'La cuisine a bien été mise à jour.',
+        type: 'success'
+      })
+      const resdata = response._data as APIResponse<Cuisine>
+
+      const index = cuisines.value.findIndex(c => c.cuisine_id === cuisineData.value.cuisine_id)
+      if (index !== -1) {
+        cuisines.value[index] = resdata.data
+      }
+
+      cuisineData.value = {} as Cuisine
+      formContent.name = ''
+      open.value = false
+    }
+  })
+
+  
+}
+
 const onClose = () => {
   formContent.name = ''
   cuisineData.value = {} as Cuisine
