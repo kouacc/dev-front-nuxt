3b8e1fd5cd51ce1c8b788e6d9b485b570a92cab1
kouacc <maxrur57@gmail.com>
2025-12-30 18:48:31 +0100

    feat(cuisine): handle modal v-model values

--- app/components/dashboard/admin/cuisines/CuisinesList.vue
+++ app/components/dashboard/admin/cuisines/CuisinesList.vue
@@ -5,6 +5,19 @@ const { data: cuisines } = await useAsyncData('cuisines', async () => {
   })
   return data
 })
+
+const modalOpen = ref<boolean>(false)
+const selectedModal = ref<'create' | 'update' | 'delete' | null>(null)
+
+const selectedCuisine = ref<Cuisine | null>(null)
+
+const openModal = (modalType: 'create' | 'update' | 'delete', cuisine?: Cuisine) => {
+  selectedModal.value = modalType
+  if (cuisine) {
+    selectedCuisine.value = cuisine
+  }
+  modalOpen.value = true
+}
 </script>
 <template>
   <div class="cuisines-list">
