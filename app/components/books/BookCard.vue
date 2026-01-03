b7bc0d7168051ea9143444c062c7be3e275760e5
kouacc <maxrur57@gmail.com>
2026-01-03 16:39:51 +0100

    feat(books): book card

---
+++ app/components/books/BookCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { Book } from '~/types/sanity.types'
+
+type BookWithCategories = Omit<Book, 'categories'> & { categories: { _key: string; title: string }[] }
+
+defineProps<{
+  book: BookWithCategories
+}>()
+</script>
+
+<template>
+</template>
