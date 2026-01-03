79ee6d3c1b07c3606bc4a44953a00c920f7dbc7b
kouacc <maxrur57@gmail.com>
2026-01-03 19:25:37 +0100

    feat(recipes): remove old template and styles, page header

--- app/pages/recipes/[id].vue
+++ app/pages/recipes/[id].vue
@@ -17,45 +17,28 @@ if (error.value) {
 </script>
 
 <template>
-  <div>
-    <UITitle type="heading1">{{ recipe?.title }}</UITitle>
-    <NuxtImg src="https://placehold.co/1000x600" :alt="recipe?.title" />
-    <div class="recipe__content">
-      <div>
-        <UITitle type="heading2">Etapes</UITitle>
-        <ul>
-          <li v-for="(step, index) in recipe?.instructions" :key="index">
-            {{ step.step_number }}. {{ step.description }}
-          </li>
-        </ul>
+  <div class="p-recipe">
+    <div class="p-recipe__header">
+      <UITitle type="heading1">{{ recipe?.title }}</UITitle>
+      <p class="p-recipe__description">{{ recipe?.description }}</p>
+    </div>
       </div>
-      <aside>
-        <UITitle type="heading2">Ingrédients</UITitle>
-        <ul>
-          <li v-for="(ing, index) in recipe?.ingredients" :key="index">
-            {{ ing.quantity }} {{ ing.unit }} de {{ ing.name }}
           </li>
-        </ul>
-      </aside>
+        </ol>
+      </div>
     </div>
   </div>
 </template> 
 
 <style lang="scss">
-.recipe__content {
-  display: flex;
-  gap: rem(32);
+.p-recipe {
+  max-width: rem(1200);
+  margin: 0 auto;
+  padding: rem(32) rem(16);
 
-  aside {
-    flex: 1;
-    max-width: rem(300);
-    padding: rem(16);
-    background-color: var(--color-primary-200);
     border-radius: rem(16);
   }
 
-  div {
-    flex: 2;
   }
 }
 </style>
